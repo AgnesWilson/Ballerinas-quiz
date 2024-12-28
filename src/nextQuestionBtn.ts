9e3ad54e56c224e6f1fd1d72f7d43f8b0262e20a
Carl400 <cmwellman@gmail.com>
2024-12-28 19:43:52 +0100

    check answer function to main
    added answer check function to main ts and removed checkAnswer.ts.
    Also added notes for nextQuestion button needs. I think we need to make sure that when next question button is clicked check answer is executed and not sooner like now.

--- src/nextQuestionBtn.ts
+++ src/nextQuestionBtn.ts
@@ -1,15 +1,15 @@
-//exchange these when real answers have been added and change the "as HTML" to TS?? 
+// exchange these when real answers have been added and change the "as HTML" to TS?? 
 const q1 = document.querySelector('#q1') as HTMLInputElement;
 const q2 = document.querySelector('#q2') as HTMLInputElement;
 const q3 = document.querySelector('#q3') as HTMLInputElement;
 
-//Activate the next-question button when the user chooses an answer
+// Activate the next-question button when the user chooses an answer
 function ifRadioBtnHasBeenChecked() {
     const nextQuestionBtn = document.querySelector('#nextQuestionBtn') as HTMLButtonElement;
 
     // IF any of the buttons q1-q3 is checked
-    //THEN make the disable button false, so it can be clicked
-    //ELSE the button is disabled and not clickable
+    // THEN make the disable button false, so it can be clicked
+    // ELSE the button is disabled and not clickable
     if (q1.checked || q2.checked || q3.checked) {
         nextQuestionBtn.disabled = false;
     } else {
@@ -17,7 +17,7 @@ function ifRadioBtnHasBeenChecked() {
     }
 }
 
-//Event-listeners for radio-btns
+// Event-listeners for radio-btns
 function activateNextQuestionBtn() {
     q1.addEventListener('click', ifRadioBtnHasBeenChecked);
     q2.addEventListener('click', ifRadioBtnHasBeenChecked);
@@ -27,7 +27,13 @@ function activateNextQuestionBtn() {
 // Exportera funktionen som default
 export default activateNextQuestionBtn;
 
-//AGNES NOTES 
+// AGNES NOTES 
 // when the questions have been printed/added go through this again and  update the q1-q3 variables 
-//Also check if the event-listeners have been added/if they should be global in the main instead 
+// Also check if the event-listeners have been added/if they should be global in the main instead 
+
+// CARL NOTES
+// The check answer function chouls most likely live here. so that the user flow becomes:
+// user chose answer with radio button -> next question button get activated -> user clicks next question buttton ->
+// functions for next question becomes: 1. checks if answer is correct 2. updates points counter 3. updates questio counter
+// 4. prints page with questions
 
